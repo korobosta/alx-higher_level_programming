84a400b1295bcc0782b2fa06949999acc7c0e5ac
Kevin Ongulu <kevinkorobosta@gmail.com>
2023-01-10 08:35:30 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-''' function that returns an object (Python data structure) represented by a JSON string
-'''
-
+"""This module contains a function that returns an
+    object (Python data structure) represented by a JSON string.
+"""
 import json
 
 
 def from_json_string(my_str):
-    ''' module from_json_string
-     returns Python objects
-    '''
+    """Returns an object (Python data structure) represented by
+        a JSON string:
+
+    Arguments:
+        my_str (obj) -- json str
+
+    Returns:
+        obj -- object
+    """
     return json.loads(my_str)
