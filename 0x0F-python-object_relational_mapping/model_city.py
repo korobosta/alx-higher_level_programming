61f674092716848296b2dd8d9a88bddaf18058f6
Kevin Ongulu <kevinkorobosta@gmail.com>
2023-03-20 06:32:22 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains a class that defines the City class.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """Definition of a City class.
+
+    Args:
+        Base : base class.
+    """
+    __tablename__ = 'cities'
+
+    id = Column('id', Integer, nullable=False,
+                autoincrement=True, unique=True, primary_key=True)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey('states.id'),
+                      nullable=False)
+    state = relationship("State")
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a class that defines the State class.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of a State class.
+
+    Args:
+        Base : base class.
+    """
+    __tablename__ = 'states'
+
+    id = Column('id', Integer, nullable=False,
+                autoincrement=True, unique=True, primary_key=True)
+    name = Column('name', String(128), nullable=False)
